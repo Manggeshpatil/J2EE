e4e415239a96dadd595a782cc71baf5dc52fe106
Manggeshpatil <166353759+Manggeshpatil@users.noreply.github.com>
2026-04-23 16:17:05 +0530

    Add files via upload

---
+++ 1ejhm7/demo/demo.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.lang.module.Configuration;
+
+public class demo {
+
+    public static void main(String[] args) {
+        Configuration cfg;
+        SessionFactory factory;
+        Session ses;
+
+        cfg = new javax.security.auth.login.Configuration();
+    }
+}
